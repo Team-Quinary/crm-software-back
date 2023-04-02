fd1ed06307a95c32dec854f61c4bd84582e71b2e
MWijesekara <menurawijesekara17@gmail.com>
2023-04-02 11:25:43 +0530

    intergration form

---
+++ crm-software-back/Services/FeedbackFormServices/IFeedackFormServices.cs
@@ -0,0 +1,10 @@
+﻿using crm_software_back.Models;
+
+namespace crm_software_back.Services.FeedbackFormServices
+{
+    public interface IFeedackFormServices
+    {
+        public Task<FeedbackForm?> PostFeedbackForm(FeedbackForm newFeedbackForm);
+        public Task<List<FeedbackForm>?> GetFeedbackForms();
+    }
+}
